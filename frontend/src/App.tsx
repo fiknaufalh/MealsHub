7e32e71a85f386b44a3b6a929d2a9019a2816314
Priscilla Auleader Napitupulu <priscillanapitupuluu@gmail.com>
2023-11-18 12:15:52 +0700

    feat(UC10): create message confirmed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
-export default function App() {
-    return (
-        <div className="bg-blue-500 grid place-content-center min-h-screen">
-            <h1 className="text-xl">Hello, world!</h1>
-        </div>
-    );
-}
+import Confirm from "./confirm.css";
+
+const App = () => {
+  const [showConfirm, setShowConfirm] = React.useState(false);
+
+  const handleConfirm = () => {
+    // Do something when the user confirms
+  };
+
+  const handleCancel = () => {
+    setShowConfirm(false);
+  };
+
+  return (
+    <div>
+      <button onClick={() => setShowConfirm(true)}>Confirm</button>
+
+      {showConfirm && (
+        <Confirm
+          message="Are you sure you want to confirm?"
+          onCancel={handleCancel}
+          onConfirm={handleConfirm}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
