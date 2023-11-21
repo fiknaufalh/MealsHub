9734dff63b053ea6c0f4c1196beb6ed33578306f
Priscilla Auleader Napitupulu <priscillanapitupuluu@gmail.com>
2023-11-21 09:12:47 +0700

    feat(UC10) : add confirn payment

--- frontend/src/pages/Confirm Payment/confirm.tsx
+++ frontend/src/pages/Confirm Payment/confirm.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 import Sidebar from '../../components/Sidebar'
 
 const Confirm = () => {
@@ -22,17 +21,6 @@ const Confirm = () => {
                     <p className ='text-md font-thin'> Please wait for tenant to finish preparing your order.</p>
                     </div>
                 </div>
-=======
-
-const Confirm = () => {
-  return (
-    <div className='flex flex-col w-full bg-[#FBD8AA] text-black min-h-screen px-16 py-8 space-y-8'>
-        <p className ='text-lg'>Welcome to <span className='font-bold'>Table 1</span></p>
-        <div className='bg-white py-8 px-16 space-y-8 rounded-xl'>
-            <p className='text-2xl font-bold text-[#E44937]'> Payment Confirmed </p>
-            <p className ='text-md font-thin'> Your payment has been confirmed. </p>
-            <p className ='text-md font-thin'> Please wait for tenant to finish preparing your order.</p>
->>>>>>> e6aac4c13729c31f92bb16adb0896c0b5b1fcd3c
         </div>
     </div>
   )
