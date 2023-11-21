5c23696c250290dff0b9406cc7a35db64f9625cc
Priscilla Auleader Napitupulu <priscillanapitupuluu@gmail.com>
2023-11-21 09:24:35 +0700

    feat(UC05) : add unique code

--- frontend/src/pages/Payment/payment.tsx
+++ frontend/src/pages/Payment/payment.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
+import Sidebar from '../../components/Sidebar'
 
 const Payment = () => {
   return (
-    <div className='flex flex-col w-full bg-[#FBD8AA] text-black min-h-screen px-16 py-8 space-y-8'>
-        <p className ='text-lg'>Welcome to <span className='font-bold'>Table 1</span></p>
-        <div className='bg-white py-8 px-16 space-y-8 rounded-xl'>
-            <p className='text-2xl font-bold text-[#E44937]'> Unique Code </p>
-            <p className ='text-1xl'> You have checked out your order. </p>
-            <p className ='text-1xl'>Here is your unique code.</p>
-            <p className ='text-4xl font-bold text-[#0B828F]'> A123 </p>
-            <p className ='text-1xl'>Don’t forget to show it to the central cashier to pay your order before timer runs out.</p>
-            <p className ='text-4xl font-bold text-[#F5A306]'>29:59</p>
+    // Create grid layout for sidebard, header, and main content
+    <div className="grid grid-cols-5 grid-rows-8 h-screen bg-mealshub-cream">
+        {/* Sidebar */}
+        <div className="col-span-1 row-span-8">
+                <Sidebar number={2} current={2} menu1="Menu" path1="M0 0H27V32H0V0ZM2.7 3.2V12H24.3V3.2H2.7ZM24.3 15.2H2.7V28.8H24.3V15.2ZM5.3946 6.4H8.1V9.6H8.0946V9.6064H5.3946V6.4ZM10.8 6.4H21.6V9.6H10.8V6.4Z" menu2="Orders" path2="M4.10306 1.10306C3 2.20612 3 3.97929 3 7.52941V24.4706C3 28.0207 3 29.7939 4.10306 30.8969C5.20612 32 6.97929 32 10.5294 32H21.8235C25.3736 32 27.1468 32 28.2499 30.8969C29.3529 29.7939 29.3529 28.0207 29.3529 24.4706V7.52941C29.3529 3.97929 29.3529 2.20612 28.2499 1.10306C27.1468 -1.12197e-07 25.3736 0 21.8235 0H10.5294C6.97929 0 5.20612 -1.12197e-07 4.10306 1.10306ZM10.5294 7.52941C10.0302 7.52941 9.5514 7.72773 9.19839 8.08074C8.84538 8.43375 8.64706 8.91253 8.64706 9.41176C8.64706 9.911 8.84538 10.3898 9.19839 10.7428C9.5514 11.0958 10.0302 11.2941 10.5294 11.2941H21.8235C22.3228 11.2941 22.8015 11.0958 23.1546 10.7428C23.5076 10.3898 23.7059 9.911 23.7059 9.41176C23.7059 8.91253 23.5076 8.43375 23.1546 8.08074C22.8015 7.72773 22.3228 7.52941 21.8235 7.52941H10.5294ZM10.5294 15.0588C10.0302 15.0588 9.5514 15.2571 9.19839 15.6102C8.84538 15.9632 8.64706 16.4419 8.64706 16.9412C8.64706 17.4404 8.84538 17.9192 9.19839 18.2722C9.5514 18.6252 10.0302 18.8235 10.5294 18.8235H21.8235C22.3228 18.8235 22.8015 18.6252 23.1546 18.2722C23.5076 17.9192 23.7059 17.4404 23.7059 16.9412C23.7059 16.4419 23.5076 15.9632 23.1546 15.6102C22.8015 15.2571 22.3228 15.0588 21.8235 15.0588H10.5294ZM10.5294 22.5882C10.0302 22.5882 9.5514 22.7866 9.19839 23.1396C8.84538 23.4926 8.64706 23.9714 8.64706 24.4706C8.64706 24.9698 8.84538 25.4486 9.19839 25.8016C9.5514 26.1546 10.0302 26.3529 10.5294 26.3529H18.0588C18.5581 26.3529 19.0368 26.1546 19.3898 25.8016C19.7429 25.4486 19.9412 24.9698 19.9412 24.4706C19.9412 23.9714 19.7429 23.4926 19.3898 23.1396C19.0368 22.7866 18.5581 22.5882 18.0588 22.5882H10.5294Z" />
         </div>
-    </div>
+        {/* Header */}
+        <div className="col-span-4">
+          <div className="ms-20">
+              <div className="row-span-1 mt-9 py-3 w-11/12">
+                <p className ='text-lg'>Welcome to <span className='font-bold'>Table 1</span></p>
+              </div>
+              <div className='bg-white py-12 px-16 space-y-8 w-11/12 rounded-xl mx'>
+              <p className='text-2xl font-bold text-[#E44937]'> Unique Code </p>
+              <p className ='text-1xl'> You have checked out your order. </p>
+              <p className ='text-1xl'>Here is your unique code.</p>
+              <p className ='text-4xl font-bold text-[#0B828F]'> A123 </p>
+              <p className ='text-1xl'>Don’t forget to show it to the central cashier to pay your order before timer runs out.</p>
+              <p className ='text-4xl font-bold text-[#F5A306]'>29:59</p>
+              </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
