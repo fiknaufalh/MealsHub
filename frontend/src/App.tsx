d6018f73fc5746ff12e2ceef4d928bd484030e56
Imanuel Raditya <16521421@mahasiswa.itb.ac.id>
2023-11-21 22:38:19 +0700

    feat(UC03): add tenant info page to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Homepage from "./pages/Homepage";
+import TenantInfo from "./pages/TenantInfo";
 
 export default function App() {
     return (
         <div>
             <Homepage />
+            {/* <TenantInfo /> */}
         </div>
     );
 }
