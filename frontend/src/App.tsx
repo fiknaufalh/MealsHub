f43bb07b2ce0a71b410140b00aaa4c778e275695
Priscilla Auleader Napitupulu <priscillanapitupuluu@gmail.com>
2023-11-22 11:18:27 +0700

    chore (UC:5) : add order summary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Confirm from './pages/Confirm Payment/confirm'
+import Summary from './pages/Payment Details/summary'
 
 const App = () => {
   return (
-    <div><Confirm /></div>
+    <div><Summary/></div>
   )
 }
 
