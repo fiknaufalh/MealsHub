bf09178e762467c3ac9948925aa34f817a4e1968
oncarrozaqy <oncarrozaqyy@gmail.com>
2023-11-23 09:34:50 +0700

    chore(UC08 & UC07): change layout and styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,25 @@
 import TenantInfo from "./pages/TenantInfo"
 import ShoppingCart from "./pages/ShoppingCart";
 import { ShoppingCartProvider } from "./contexts/ShoppingCartContext";
+import PageManageOrder from "./pages/PageManageOrder";
+import PageManageMenu from "./pages/PageManageMenu";
+import OrderSummary from "./pages/OrderSummary";
+import OrderDetails from "./pages/OrderDetailsTenant";
+import Homepage from "./pages/Homepage";
 
 export default function App() {
     return (
         <div>
-            <ShoppingCartProvider>
+            {/* <ShoppingCartProvider>
                 <TenantInfo />
-                {/* <ShoppingCart /> */}
-            </ShoppingCartProvider>
+                <ShoppingCart />
+            </ShoppingCartProvider> */}
+            <PageManageMenu />
+            {/* <PageManageOrder /> */}
+            {/* <OrderSummary /> */}
+            {/* <OrderDetails /> */}
+            {/* <Homepage /> */}
         </div>
     );
 }
+
