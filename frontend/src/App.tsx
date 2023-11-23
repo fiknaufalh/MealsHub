d798c0cbc63db1d37425b244d6bdbc1a17c0c781
Fikri Naufal Hamdi <18221096@std.stei.itb.ac.id>
2023-11-23 11:33:30 +0700

    feat: adjust App, AppRoutes, and main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
-import TenantInfo from "./pages/TenantInfo"
+import TenantInfo from "./pages/TenantInfo";
 import ShoppingCart from "./pages/ShoppingCart";
+import AppRoutes from "./AppRoutes";
+import Login from "./pages/LoginPage";
 import { ShoppingCartProvider } from "./contexts/ShoppingCartContext";
 
 export default function App() {
     return (
-        <div>
-            <ShoppingCartProvider>
-                <TenantInfo />
-                <ShoppingCart />
-            </ShoppingCartProvider>
-        </div>
+        <>
+            <AppRoutes />
+        </>
     );
 }
