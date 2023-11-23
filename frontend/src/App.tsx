459f1aefe8ef9ddb1b7bc6e58801afafcbe1295e
Priscilla Auleader Napitupulu <priscillanapitupuluu@gmail.com>
2023-11-23 13:56:49 +0700

    chore (UC05) : edit side bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
-import TenantInfo from "./pages/TenantInfo"
-import ShoppingCart from "./pages/ShoppingCart";
-import { ShoppingCartProvider } from "./contexts/ShoppingCartContext";
-=======
-import Sidebar from "./components/Sidebar";
-import TenantCard from "./components/TenantCard";
-import Search from "./components/Search";
-//import ProductCard from "./components/ProductCard";
 import Payment from "./pages/Payment/payment";
-
->>>>>>> feat(UC05) : add make payment
 
 export default function App() {
     return (
         <div>
-<<<<<<< HEAD
-            <ShoppingCartProvider>
-                <TenantInfo />
-                {/* <ShoppingCart /> */}
-            </ShoppingCartProvider>
-=======
             <Payment />
->>>>>>> feat(UC05) : add make payment
         </div>
     );
 }
