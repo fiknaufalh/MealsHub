4be9b13404ce2fe639fc978468ee803ac4c900e6
Fikri Naufal Hamdi <18221096@std.stei.itb.ac.id>
2023-11-23 09:07:26 +0700

    feat: add supabase fetching data api

---
+++ backend/src/services/api/apiTenant.ts
@@ -0,0 +1,25 @@
+import supabase from "../supabase.js";
+
+export async function getTenant() {
+    const { data, error } = await supabase
+        .from("tenant")
+        .select("*")
+        // Filters
+        .eq("column", "Equal to")
+        .gt("column", "Greater than")
+        .lt("column", "Less than")
+        .gte("column", "Greater than or equal to")
+        .lte("column", "Less than or equal to")
+        .like("column", "%CaseSensitive%")
+        .ilike("column", "%CaseInsensitive%")
+        .is("column", null)
+        .in("column", ["Array", "Values"])
+        .neq("column", "Not equal to");
+
+    if (error) {
+        console.log(error);
+        throw new Error("Order could not be loaded.");
+    }
+
+    return data;
+}
