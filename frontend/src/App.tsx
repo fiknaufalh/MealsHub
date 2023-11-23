f803a7cbc530e693358b51589a54288e05511a3a
Priscilla Auleader Napitupulu <priscillanapitupuluu@gmail.com>
2023-11-23 18:59:57 +0700

    chore (UC10) : edit app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 import Payment from "./pages/Payment/payment";
 
-=======
-import Sidebar from "./components/Sidebar";
-import TenantCard from "./components/TenantCard";
-import Search from "./components/Search";
-//import ProductCard from "./components/ProductCard";
-import Payment from "./pages/Payment/payment";
-
-
->>>>>>> 5c23696c250290dff0b9406cc7a35db64f9625cc
 export default function App() {
     return (
         <div>
