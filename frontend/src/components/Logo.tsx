079004eaf22ba1b4d215679e1de923a1bca3fdd5
Imanuel Raditya <16521421@mahasiswa.itb.ac.id>
2023-11-24 19:02:43 +0700

    fix: rename react attribute

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 export default function Logo() {
     return (
             <img src = "../../public/images/MealsHub.png" alt = "MealsHub" className="flex object-cover h-full w-full" />
