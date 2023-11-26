ebf3f2e2ed23d785aa2301110b88be68009ddab2
oncarrozaqy <oncarrozaqyy@gmail.com>
2023-11-26 22:51:27 +0700

    fix: fix bug confirm payment (cashier)

--- frontend/src/pages/ManagePayment.tsx
+++ frontend/src/pages/ManagePayment.tsx
@@ -55,8 +55,9 @@ interface OrderDetails {
     paymentstatus: string;
 }
 
-const Summary = () => {
+const ManagePayment = () => {
     const { orderid } = useParams();
+    const paymentid = orderid;
     const [confirmed, setConfirmed] = useState(false);
     const [showProfileDropDown, setShowProfileDropDown] = useState(false);
     const [showConfirmPopUp, setShowConfirmPopUp] = useState(false);
@@ -67,7 +68,7 @@ const Summary = () => {
         label: "Waiting for Payment",
         disabled: true,
         color: "mealshub-greenpalet",
-        onClick: () => {},
+        onClick: () => { },
     });
     const getOrderDetailsData = async () => {
         const orderResponse = await Axios.get(
@@ -108,12 +109,11 @@ const Summary = () => {
     };
 
     const handleConfirmPayment = async () => {
-        setShowConfirmPopUp(true);
 
         // Panggil API untuk memperbarui status order menjadi "Confirmed" atau "Completed"
         try {
             console.log("BELOM");
-            await Axios.patch(`http://localhost:8000/orders/${orderid}`, {
+            await Axios.patch(`http://localhost:8000/payments/${paymentid}`, {
                 status: "Confirmed", // Ubah status order ke "Confirmed"
             });
             console.log("MAASUK");
@@ -126,7 +126,7 @@ const Summary = () => {
                 label: "Confirmed", // Ganti label tombol menjadi "Confirmed"
                 disabled: true,
                 color: "mealshub-greenpalet",
-                onClick: () => {}, // Fungsi kosong karena tombol akan dinonaktifkan setelah diklik
+                onClick: () => { }, // Fungsi kosong karena tombol akan dinonaktifkan setelah diklik
             });
         } catch (error) {
             // Handle error jika terjadi kesalahan saat memanggil API
@@ -134,34 +134,6 @@ const Summary = () => {
             // Tambahkan penanganan error sesuai kebutuhan aplikasi Anda
         }
     };
-
-    const handleCloseConfirmPopUp = async () => {
-        setShowConfirmPopUp(false);
-    
-        try {
-            // Panggil API untuk memperbarui status order menjadi "Confirmed"
-            await Axios.patch(`http://localhost:8000/orders/${orderid}`, {
-                status: "Completed",
-            });
-    
-            // Dapatkan ulang data order setelah perubahan status
-            getOrderDetailsData();
-    
-            // Atur ulang button state setelah konfirmasi
-            setButtonState({
-                label: "Confirmed",
-                disabled: true,
-                color: "mealshub-greenpalet",
-                onClick: () => {}, // Tombol dinonaktifkan karena status sudah "Confirmed"
-            });
-    
-            setConfirmed(true); // Setelah menutup pop-up, tandai bahwa konfirmasi telah dilakukan
-        } catch (error) {
-            console.error("Error confirming payment:", error);
-            // Tambahkan penanganan error sesuai kebutuhan aplikasi Anda
-        }
-    };
-    
 
     const getOrderSummaryData = async () => {
         const orderResponse = await Axios.get(
@@ -231,14 +203,14 @@ const Summary = () => {
                     label: "Confirm",
                     disabled: false,
                     color: "mealshub-red",
-                    onClick: () => setShowConfirmPopUp(true), // Menampilkan pop-up saat tombol diklik
+                    onClick: () => handleConfirmPayment(), // Menampilkan pop-up saat tombol diklik
                 });
             } else if (paymentStatus === "Confirmed") {
                 setButtonState({
                     label: "Confirmed",
                     disabled: true,
                     color: "mealshub-greenpalet",
-                    onClick: () => {}, // Tombol dinonaktifkan karena status sudah "Confirmed"
+                    onClick: () => { }, // Tombol dinonaktifkan karena status sudah "Confirmed"
                 });
             }
         }
@@ -254,6 +226,7 @@ const Summary = () => {
                     current={1}
                     menu1="Payment List"
                     path1="M8.22042 7.85718C6.79962 7.85718 5.43701 8.47803 4.43235 9.58316C3.42769 10.6883 2.86328 12.1872 2.86328 13.75V14.9286H37.1433V13.75C37.1433 12.1872 36.5789 10.6883 35.5742 9.58316C34.5696 8.47803 33.2069 7.85718 31.7861 7.85718H8.22042ZM2.86328 30.25V17.2857H37.1433V30.25C37.1433 31.8129 36.5789 33.3118 35.5742 34.4169C34.5696 35.522 33.2069 36.1429 31.7861 36.1429H8.22185C6.80105 36.1429 5.43844 35.522 4.43378 34.4169C3.42912 33.3118 2.86471 31.8129 2.86471 30.25H2.86328ZM26.0719 25.9286C25.7877 25.9286 25.5152 26.0528 25.3142 26.2738C25.1133 26.4948 25.0004 26.7946 25.0004 27.1072C25.0004 27.4198 25.1133 27.7195 25.3142 27.9406C25.5152 28.1616 25.7877 28.2857 26.0719 28.2857H31.0719C31.356 28.2857 31.6285 28.1616 31.8295 27.9406C32.0304 27.7195 32.1433 27.4198 32.1433 27.1072C32.1433 26.7946 32.0304 26.4948 31.8295 26.2738C31.6285 26.0528 31.356 25.9286 31.0719 25.9286H26.0719Z"
+                    page1="/cashier/payments"
                 />
             </div>
             {/* Header */}
@@ -277,7 +250,7 @@ const Summary = () => {
                 </div>
                 <div className="row-span-7 mt-6 mb-9 w-11/12">
                     <div className="ms-4">
-                        <BackButton />
+                        <BackButton page="/cashier/payments" />
                     </div>
                     <div className="ms-20 py-12 bg-white rounded-3xl">
                         <h2 className="text-mealshub-red text-3xl font-bold ms-16">
@@ -289,18 +262,15 @@ const Summary = () => {
                             <button
                                 onClick={buttonState.onClick}
                                 disabled={buttonState.disabled}
-                                className={`max-w-xs text-white font-bold text-lg rounded-full px-2 py-1 text-center shadow-xl bg-${buttonState.color}`}
+                                className={`mt-4 px-6 py-2 w-1/4 text-white text-lg font-nunito font-semibold shadow-xl rounded-full bg-${buttonState.color}`}
                             >
                                 {buttonState.label}
                             </button>
                         </div>
-                        {showConfirmPopUp && (
-                            <ConfirmPopUp onClose={handleCloseConfirmPopUp} />
-                        )}
                     </div>
                 </div>
             </div>
         </div>
     );
 };
-export default Summary;
+export default ManagePayment;
