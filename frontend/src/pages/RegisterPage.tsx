36cb6900f56a0149c985b1a23d786a9b1e2309ce
Fikri Naufal Hamdi <18221096@std.stei.itb.ac.id>
2023-11-27 22:15:05 +0700

    feat(UC02): add back button in register

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,6 +1,12 @@
 import RegisterForm from "../components/RegisterForm";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1);
+    };
+
     return (
         <div className="grid grid-cols-2 min-h-screen">
             <div className="col-span-1 relative">
@@ -13,9 +19,15 @@ export default function RegisterPage() {
                     <h1 className="text-9xl font-bold text-white drop-shadow-2xl">
                         JOIN US
                     </h1>
-                    <span className="bg-mealshub-blue text-white font-nunito font-bold text-lg my-16 px-24 py-2">
+                    <div className="bg-mealshub-blue text-white font-nunito font-bold text-lg mt-16 mb-8 px-24 py-2">
                         Discover a new way to dine!
-                    </span>
+                    </div>
+                    <button
+                        onClick={handleGoBack}
+                        className="bg-mealshub-orange text-white px-4 py-2 rounded-lg hover:bg-mealshub-cream focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium text-sm"
+                    >
+                        Go Back
+                    </button>
                 </div>
             </div>
             <div className="flex items-center justify-center bg-mealshub-cream col-span-1">
